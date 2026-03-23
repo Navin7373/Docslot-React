333eee907c0705713d96d9efb0d07fa389c7f13c
Navin7373 <naveenselvaraj416@gmail.com>
2026-03-23 08:14:00 +0530

    third commit

--- src/HomeNav.jsx
+++ src/HomeNav.jsx
@@ -1,17 +1,29 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { useEffect, useState } from "react";
 import photo from "./assets/logo.png";
 import { Link, useLocation } from "react-router-dom";
 
 function HomeNav() {
-
   const location = useLocation();
+  const [expanded, setExpanded] = useState(false);
+
+  useEffect(() => {
+    setExpanded(false);
+  }, [location.pathname]);
+
+  const closeNavbar = () => setExpanded(false);
 
   return (
-    <Navbar expand="lg" className="bg-info-subtle  py-3 fixed-top">
+    <Navbar
+      expand="lg"
+      expanded={expanded}
+      onToggle={(nextExpanded) => setExpanded(nextExpanded)}
+      className="bg-info-subtle  py-3 fixed-top"
+    >
       <Container>
-        <Nav.Link as={Link} to="/" className="py-0">
+        <Nav.Link as={Link} to="/" className="py-0" onClick={closeNavbar}>
           <img src={photo} width={150} height={50} className="navbar-brand-img" alt="Book-It logo" />
         </Nav.Link>
 
@@ -22,33 +34,33 @@ function HomeNav() {
 
             {/* Hide Home if already on Home */}
             {location.pathname !== "/" && (
-              <Nav.Link as={Link} to="/">Home</Nav.Link>
+              <Nav.Link as={Link} to="/" onClick={closeNavbar}>Home</Nav.Link>
             )}
 
             {/* Hide Doctors if already on Doctors OR Booking page */}
             {!location.pathname.startsWith("/doctors") &&
               !location.pathname.startsWith("/book") && (
-                <Nav.Link as={Link} to="/doctors">
+                <Nav.Link as={Link} to="/doctors" onClick={closeNavbar}>
                   Book Appointment
                 </Nav.Link>
               )}
 
             {/* Hide My Appointments if already there */}
             {location.pathname !== "/my-appointments" && (
-              <Nav.Link as={Link} to="/my-appointments">
+              <Nav.Link as={Link} to="/my-appointments" onClick={closeNavbar}>
                 My Appointments
               </Nav.Link>
             )}
 
             {/* Hide Profile if already there */}
             {location.pathname !== "/profile" && (
-              <Nav.Link as={Link} to="/profile">
+              <Nav.Link as={Link} to="/profile" onClick={closeNavbar}>
                 Profile
               </Nav.Link>
             )}
 
             {location.pathname !== "/login" && (
-              <Nav.Link as={Link} to="/login">
+              <Nav.Link as={Link} to="/login" onClick={closeNavbar}>
                 login
               </Nav.Link>
             )}
